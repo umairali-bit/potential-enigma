31cdbf33eedbeb067041a6496565cff7098837d6
Umair Ali <umairmamoor@gmail.com>
2021-08-16 20:22:26 -0400

    wrtieToFile function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // // TODO: Include packages needed for this application
-//const fs = require('fs');
-//const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
 
 
@@ -9,25 +9,69 @@ const inquirer = require('inquirer');
  const questions = [
   {
     type: 'input',
-    name: 'name',
-    message: 'What is your name?'
-  }
+    name: 'title',
+    message: 'What is the title of your project'
+  },
+  {
+    type: 'input',
+    name: 'about',
+    message: 'Please describe your project'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How would you install the project'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Describe usage of the project'
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Please choose a license for the project',
+    choices: ['MIT', 'Apache License 2.0', 'Mozilla Public License 2.0']
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username'
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Who contributed to this project?'
+  },
+  {
+    type: 'input',
+    name: 'Test',
+    message: 'Please write a test for your application'
+  },
+
  ];
 
-// // TODO: Create a function to write README file
-//  function writeToFile(fileName, data) {   
-//    fs.writeFile('index.html', generateMarkdown(name, github), err => {
-//   if (err) throw err;
+// TODO: Create a function to write README file
+ function writeToFile(fileName, data) {   
+   fs.writeFile('readme.md', (fileName,data), err => {
+  if (err) throw err;
   
-//    console.log('Portfolio complete! Check out index.html to see the output!');
-//    });}
+   console.log('Portfolio complete! Check out index.html to see the output!');
+});}
 
 // // TODO: Create a function to initialize app
  function init() {
    return inquirer.prompt(questions)
-   .then(answers => console.log(answers));
+   .then(data => {
+     writeToFile('README.md', generateMarkdown(data));
 
-}
+})
+ }
 
 // // Function call to initialize app
  init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,35 +10,23 @@
 // // If there is no license, return an empty string
 // function renderLicenseSection(license) {}
 
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
+// TODO: Create a function to generate markdown for README
+//  function generateMarkdown(data) {
+//    return `# ${data.title}
 
-// `;
-// }
+//  `;
+//  }
 
  
 
 
-
-const generateMarkdown= (name, github) => {
+module.exports = data =>{
+console.log(data);
     return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
+    # ${data.title}  
+    
     `;
   };
 
-  module.exports = generateMarkdown;
+  
 
